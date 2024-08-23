553c890e82f15f80200180d738dc98c6235ae158
Hussain Calcuttawala <hussain.c@peopleinteractive.in>
2024-08-23 22:20:44 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,191 @@
+'use client'
+import { Button, message, Upload, Table, Popconfirm } from 'antd';
 import Image from "next/image";
+import { UploadOutlined } from '@ant-design/icons';
+import { useState } from "react";
+import axios from 'axios';
+import { exportToExcel } from 'react-json-to-excel';
+
 
 export default function Home() {
+  const [gstFileList, setGstFileList] = useState([]);
+  const [tallyFileList, setTallyFileList] = useState([]);
+  const [tableData, setTableData] = useState([]);
+
+  const props = {
+    accept: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    onChange(info: any) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  const handleUpload = async () => {
+    if (gstFileList.length === 0) {
+      message.error('Please select a gst file first!');
+      return;
+    }
+
+    if (tallyFileList.length === 0) {
+      message.error('Please select a tally file first!');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('tallyFile', tallyFileList[0]);
+    formData.append('gstFile', gstFileList[0]);
+
+    try {
+      const response = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      message.success('File uploaded and processed successfully!');
+      console.log(response.data)
+      setTableData(response.data);
+    } catch (error) {
+      message.error('Upload failed!');
+      console.error('Error:', error);
+    }
+  };
+
+  const handleDownload = () => {
+  }
+
+  const columns = [
+    {
+      title: 'GSTIN',
+      dataIndex: 'gst',
+      key: 'gst',
+    },
+    {
+      title: 'Invoie No.',
+      dataIndex: 'inv',
+      key: 'inv',
+    },
+    {
+      title: 'Taxable Value',
+      dataIndex: 'tv',
+      key: 'tv',
+    },
+    {
+      title: 'IGST',
+      dataIndex: 'igst',
+      key: 'igst',
+    },
+    {
+      title: 'SGST',
+      dataIndex: 'sgst',
+      key: 'sgst',
+    },
+    {
+      title: 'CGST',
+      dataIndex: 'cgst',
+      key: 'cgst',
+    },
+
+  ];
+
+  const handleSendEmail = (record) => {
+    setTimeout(() => message.success(`Email successfully sen to ${record.gst}`), 1000
+    )
+  }
+
+  const otherColumns = [...columns, {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    render: (_, record) => <Popconfirm title={`Are you sure you want to send an email to ${record.gst}?`} onConfirm={() => handleSendEmail(record)}>
+      <a>Send Email</a>
+    </Popconfirm>
+  },]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='mx-10'>
+      <main className="flex  flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+
+          <Upload {...props} onRemove={(file) => {
+            gstFileList([]);
+          }}
+            beforeUpload={(file) => {
+              setGstFileList([file]);
+              return false; // Prevent auto upload
+            }}
+            fileList={gstFileList}>
+            <Button icon={<UploadOutlined />}>Click to Upload GST excel</Button>
+          </Upload>
+          <Upload {...props} onRemove={(file) => {
+            tallyFileList([]);
+          }}
+            beforeUpload={(file) => {
+              setTallyFileList([file]);
+              return false; // Prevent auto upload
+            }}
+            fileList={tallyFileList}>
+            <Button icon={<UploadOutlined />}>Click to Upload Tally excel</Button>
+          </Upload>
+          <Button
+            type="primary"
+            onClick={handleUpload}
+            disabled={gstFileList.length === 0 || tallyFileList.length === 0}
+            style={{ marginTop: 16 }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            Upload and Process
+          </Button>
+        </div >
+
+      </main >
+      <div className='flex justify-end'>
+        <Button
+          type="primary"
+          onClick={() => exportToExcel([{
+            sheetName: "Exact Matches",
+            details: tableData.exact
+          },
+          {
+            sheetName: "Approx Matches",
+            details: tableData.approx
+          },
+          {
+            sheetName: "Mismatch",
+            details: tableData.mismatch
+          },
+          {
+            sheetName: "Not Found",
+            details: tableData.not_found
+          },
+
+          ], 'downloadfilename', true)}
+          style={{ marginTop: 16 }}
+        >
+          Export Data
+        </Button>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <>
+        <div>Exact Match</div>
+        {tableData && <Table dataSource={tableData.exact} columns={columns} />}
+      </>
+      <>
+        <div>Approx Match</div>
+        {tableData && <Table dataSource={tableData.approx} columns={otherColumns} />}
+      </>
+      <>
+        <div>Mismatch</div>
+        {tableData && <Table dataSource={tableData.mismatch} columns={otherColumns} />}
+      </>
+      <>
+        <div>Not Found</div>
+        {tableData && <Table dataSource={tableData.not_found} columns={otherColumns} />}
+      </>
+    </div>
   );
 }
---
+++ src/app/upload/route.ts
@@ -0,0 +1,127 @@
+// Import necessary modules
+import { NextResponse } from "next/server";
+import path from "path";
+import { writeFile } from "fs/promises";
+import xlsx from 'node-xlsx';
+
+// Define the POST handler for the file upload
+export const POST = async (req, res) => {
+    let exact = []
+    let mismatch = []
+    let not_found = []
+    let approx = []
+
+    // Parse the incoming form data
+    const formData = await req.formData();
+
+    // Get the file from the form data
+    const tallyFile = formData.get("tallyFile");
+    const gstFile = formData.get("gstFile");
+
+    // Check if a file is received
+    if (!tallyFile) {
+        // If no file is received, return a JSON response with an error and a 400 status code
+        return NextResponse.json({ error: "No tally File received." }, { status: 400 });
+    }
+    if (!gstFile) {
+        // If no file is received, return a JSON response with an error and a 400 status code
+        return NextResponse.json({ error: "No gst File received." }, { status: 400 });
+    }
+    // Convert the file data to a Buffer
+    const tallyBuffer = Buffer.from(await tallyFile.arrayBuffer());
+    const gstBuffer = Buffer.from(await gstFile.arrayBuffer());
+
+    // Replace spaces in the file name with underscores
+    const tallyFilename = tallyFile.name.replaceAll(" ", "_");
+    const gstFilename = gstFile.name.replaceAll(" ", "_");
+
+    console.log(tallyFilename, gstFilename);
+
+    try {
+        const tallyFilePath = path.join(process.cwd(), "public/assets/" + tallyFilename);
+        const gstFilePath = path.join(process.cwd(), "public/assets/" + gstFilename);
+
+
+        // Write the file to the specified directory (public/assets) with the modified filename
+        await writeFile(
+            tallyFilePath,
+            tallyBuffer
+        );
+        await writeFile(
+            gstFilePath,
+            gstBuffer
+        );
+
+        // Parse the Excel file using node-xlsx
+        const tallyWorksheet = xlsx.parse(tallyFilePath)?.[0]?.data;
+        const gstWorksheet = xlsx.parse(gstFilePath)?.[0]?.data;
+
+        let tallyData = {}
+        let gstData = {};
+
+        for (let i = 1; i < tallyWorksheet.length; i++) {
+            tallyData[`${tallyWorksheet[i][5]}${tallyWorksheet[i][8]}`] = {
+                gst: tallyWorksheet[i][5],
+                inv: tallyWorksheet[i][8],
+                tv: tallyWorksheet[i][10],
+                igst: tallyWorksheet[i][11],
+                cgst: tallyWorksheet[i][12],
+                sgst: tallyWorksheet[i][13],
+            }
+        }
+
+        for (let i = 1; i < gstWorksheet.length; i++) {
+            gstData[`${gstWorksheet[i][1]}${gstWorksheet[i][3]}`] = {
+                gst: gstWorksheet[i][1],
+                inv: gstWorksheet[i][3],
+                tv: gstWorksheet[i][9],
+                igst: gstWorksheet[i][10],
+                cgst: gstWorksheet[i][11],
+                sgst: gstWorksheet[i][12],
+            }
+        }
+
+        const large = Object.keys(tallyData).length > Object.keys(gstData).length ? tallyData : gstData;
+        const small = Object.keys(tallyData).length < Object.keys(gstData).length ? tallyData : gstData;
+
+
+        for (const key in large) {
+            let largeValue = large[key]
+            let smallValue = small[key]
+
+
+            if (typeof smallValue !== 'undefined') {
+                if (smallValue.gst == largeValue.gst && smallValue.inv == largeValue.inv) {
+                    if ((smallValue.cgst == largeValue.cgst && smallValue.sgst == largeValue.sgst) || smallValue.igst == largeValue.igst) {
+                        exact.push(largeValue)
+                    }
+                    else {
+                        mismatch.push(largeValue)
+                    }
+                    delete small[key]
+                }
+            }
+            else {
+                let f = Object.values(small).filter(x => x.gst == largeValue.gst && x.tv == largeValue.tv && ((x.cgst == largeValue.cgst && x.sgst == largeValue.sgst) || x.igst == largeValue.igst))
+                if (f.length > 0) {
+                    for (let i = 0; i < f.length; i++) {
+                        approx.push(f[i])
+                    }
+                }
+                else {
+                    not_found.push(largeValue)
+                }
+
+            }
+
+        }
+
+
+        // Return a JSON response with a success message and a 201 status code
+        return NextResponse.json({ Message: "Success", status: 201, exact, mismatch, approx, not_found });
+    } catch (error) {
+        // If an error occurs during file writing, log the error and return a JSON response with a failure message and a 500 status code
+        console.log("Error occurred ", error);
+        return NextResponse.json({ Message: "Failed", status: 500 });
+    }
+};
